47931c67ab7195979ed4570200e92ddee2934258
leejiyoung <wldud5117@naver.com>
2023-01-30 16:45:54 +0900

    step10 연습

--- step10viewpager_practice/src/main/java/com/example/step10viewpager_practice/ui/main/PageViewModel.java
+++ step10viewpager_practice/src/main/java/com/example/step10viewpager_practice/ui/main/PageViewModel.java
@@ -8,18 +8,18 @@ import androidx.lifecycle.ViewModel;
 
 public class PageViewModel extends ViewModel {
     //수정이 가능한 Live Data(MutableLiveData 객체)
-    private MutableLiveData<Integer> mIndex = new MutableLiveData<>();
+    private MutableLiveData<String> ownerName = new MutableLiveData<>();
 
     //읽기 전용 LiveData(LiveData객체) -> 이것이 viewPager에 출력되어 우리 눈에 보이게 되는 것이다,
-    private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
+    private LiveData<String> mText = Transformations.map(ownerName, new Function<String, String>() {
         @Override
-        public String apply(Integer input) {
-            return "Hello world from section: " + input;
+        public String apply(String input) {
+            return "이 구역의 주인은: " + input;
         }
     });
 
-    public void setIndex(int index) {
-        mIndex.setValue(index);
+    public void setOwnerName(String newOwner) {
+        ownerName.setValue(newOwner);
     }
 
     public LiveData<String> getText() {
